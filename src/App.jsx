a04185e7e8c7b926ac98eece85ce64bb3b5312f4
Tafita Hery <tafitahery@gmail.com>
2022-11-27 15:45:29 +0300

    Personal Information Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Intro from './components/into/Intro';
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Intro />
+    </div>
+  );
 }
 
 export default App;
