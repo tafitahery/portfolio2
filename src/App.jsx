e1b5f0468dd1b9d1dd9b79d7f7457054329ebbdb
Tafita Hery <tafitahery@gmail.com>
2022-11-28 17:46:42 +0300

    Portfolio About Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import About from './components/about/About';
 import Intro from './components/into/Intro';
 
 function App() {
   return (
     <div>
       <Intro />
+      <About />
     </div>
   );
 }
