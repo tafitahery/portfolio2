ebf1b1b405e6df2e39c96f37efc86aac4a992746
Tafita Hery <tafitahery@gmail.com>
2022-11-29 17:12:03 +0300

    Portfolio List Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import About from './components/about/About';
 import Intro from './components/into/Intro';
+import ProductList from './components/productList/ProductList';
 
 function App() {
   return (
     <div>
       <Intro />
       <About />
+      <ProductList />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: 'https://cdn.pixabay.com/photo/2017/05/11/11/15/workplace-2303851_960_720.jpg',
+    link: 'http://lama.dev',
+  },
+  {
+    id: 2,
+    img: 'https://cdn.pixabay.com/photo/2017/08/06/19/33/eyewear-2595549_960_720.jpg',
+    link: 'http://lama.dev',
+  },
+  {
+    id: 3,
+    img: 'https://cdn.pixabay.com/photo/2017/07/13/08/28/abstract-2499660_960_720.jpg',
+    link: 'http://lama.dev',
+  },
+  {
+    id: 4,
+    img: 'https://cdn.pixabay.com/photo/2016/07/11/21/23/water-lily-1510707__340.jpg',
+    link: 'http://lama.dev',
+  },
+  {
+    id: 5,
+    img: 'https://cdn.pixabay.com/photo/2017/01/18/17/14/girl-1990347__340.jpg',
+    link: 'http://lama.dev',
+  },
+  {
+    id: 6,
+    img: 'https://cdn.pixabay.com/photo/2016/02/17/19/08/lotus-1205631__340.jpg',
+    link: 'http://lama.dev',
+  },
+];
